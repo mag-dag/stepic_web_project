aeec37ff0adf6c51a0f7ed54d168fea2d261d8f2
mag-dag <nikitontitov@gmail.com>
2021-02-02 17:48:40 +0300

    My Commit

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-available/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-available/defaul
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
