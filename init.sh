70a5861e083fcc72177cdc3d120bc807d8835066
mag-dag <nikitontitov@gmail.com>
2021-02-02 17:31:43 +0300

    My commit

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-available/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
