0bb080298562822468d67a87a9729f85a15e30c3
mag-dag <nikitontitov@gmail.com>
2021-02-04 15:56:43 +0300

    1

--- web/init.sh
+++ web/init.sh
@@ -1,7 +1,7 @@
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-available/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-available/defaul
+sudo rm /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
